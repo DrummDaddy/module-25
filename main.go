7bd6fd9b9bc6c326b9868d86f8359e2ee6c267ac
Alexander <alexpospelov87@gmail.com>
2024-07-10 15:13:33 +0700

    Исправление: обновленные текстовые сообщения для принятия произвольных данных

--- main.go
+++ main.go
@@ -9,10 +9,10 @@ import (
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Введите целое число: ") // Ошибочное сообщение
+	fmt.Print("Введите данные: ") // Исправленное сообщение
 	input, err := reader.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Вы ввели число: %s", input) // Ошибочное сообщение
+	fmt.Printf("Вы ввели следующие данные: %s", input) // Исправленное сообщение
 }
